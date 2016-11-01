0d49390c1f82a6d3e9ceb6bb6feea58179d75619
J-C <jcjoneskern@gmail.com>
2016-11-01 11:58:04 -0400

    logs conversions in 3 different ways

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-//bonus ver 1: console.log(); in the function
+//basic conversion
+function convertTemp(temp,units) {
+	if (units==="C"||units==="c") {
+		return (temp-32)*(5/9);
+	} else if (units==="F"||units==="f") {
+		return temp*(9/5)+32;
+	} else {
+		console.log("Valid parameters for units are only F or C.");
+	}
+}
+//"or" is used above so that input is not case sensitive
+
+console.log(convertTemp(212, "c"));
+console.log(convertTemp(32, "C"));
+console.log(convertTemp(65, "C"));
+console.log(convertTemp(-40, "F"));
+//proof that the code is able to log all 4 arguments at the same time
+
+//bonus ver 1: console.log() in the function
 function convertTemp(temp,units) {
 	if (units==="C"||units==="c") {
 		console.log(temp+" degrees F is "+(temp-32)*(5/9)+" degrees C");
@@ -6,29 +24,36 @@ function convertTemp(temp,units) {
 	} else if (units==="F"||units==="f") {
 		console.log(temp+" degrees C is "+(temp*(9/5)+32)+" degrees F");
 		return temp*(9/5)+32;
-	} else {
+	} else {http://jsbin.com/yofameniqe/edit?js,console
 		console.log("Valid parameters for units are only F or C.");
 	}
 }
-
-//"or" is used above so that input is not case sensitive
 
 convertTemp(212, "C");
 convertTemp(32, "C");
 convertTemp(65, "c");
 convertTemp(-40, "f");
 
-//bonus ver 2: console.log(); at the end
+//bonus ver 2: console.log() outside of the function && logs a specific sentence rather than just a number
 function convertTemp(temp,units) {
 	if (units==="C"||units==="c") {
 		return (temp-32)*(5/9);
 	} else if (units==="F"||units==="f") {
 		return temp*(9/5)+32;
+	}
+}
+
+function printConvertTemp(temp,units) {
+	if (units==="C"||units==="c") {
+		console.log(temp+" degrees F is "+convertTemp(temp,units)+" degrees C");
+	} else if (units==="F"||units==="f") {
+		console.log(temp+" degrees C is "+convertTemp(temp,units)+" degrees F");
 	} else {
 		console.log("Valid parameters for units are only F or C.");
 	}
 }
 
-var temperature = convertTemp(212,"C");
-
-console.log(temperature);
+printConvertTemp(212, "C");
+printConvertTemp(32, "c");
+printConvertTemp(65, "c");
+printConvertTemp(-40, "f");
