4730a18bc295bdf1a46777337daa6be1595172b8
J-C <jcjoneskern@gmail.com>
2016-11-01 10:42:30 -0400

    message at end, but not specific yet

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ convertTemp(32, "C");
 convertTemp(65, "c");
 convertTemp(-40, "f");
 
+//bonus ver 2: console.log(); at the end
+function convertTemp(temp,units) {
+	if (units==="C"||units==="c") {
+		return (temp-32)*(5/9);
+	} else if (units==="F"||units==="f") {
+		return temp*(9/5)+32;
+	} else {
+		console.log("Valid parameters for units are only F or C.");
+	}
+}
+
+var temperature = convertTemp(212,"C");
+
+console.log(temperature);
